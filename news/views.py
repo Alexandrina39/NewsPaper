f6111aabb7b0af03d63c2c5aa1aa532b525ad3ae
Alexandrina39 <149896037+Alexandrina39@users.noreply.github.com>
2024-05-01 01:05:27 +0300

    Initial commit

---
+++ news/views.py
@@ -0,0 +1,38 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+# class NewsList(ListView):
+#     model = Post
+#     ordering = '-creationDate'
+#     template_name = 'NewsList.html'
+#     context_object_name = 'news'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['no_new'] = None
+#         return context
+from django.views.generic import ListView
+from .models import Post
+
+
+from django.views.generic import ListView
+from .models import Post
+
+
+class NewsList(ListView):
+    model = Post
+    ordering = '-created_at'  # Corrected ordering field
+    template_name = 'NewsList.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['no_new'] = None
+        return context
+
+
+class NewDetail(DetailView):
+    model = Post
+    template_name = 'New.html'
+    context_object_name = 'new'
